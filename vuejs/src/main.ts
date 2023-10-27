3cf322e46113e0e51c67fdf5c7dba7d002e9d105
Jhana <mrgamagejhana@gmail.com>
2023-10-27 16:40:55 +0200

    Update directories

--- vuejs/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-
-import { faRotateRight } from "@fortawesome/free-solid-svg-icons"
-library.add(faRotateRight);
-
-createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
