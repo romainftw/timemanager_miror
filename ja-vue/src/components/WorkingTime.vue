04a36590d9cffe0ba64adfc8caa009a5f9bfa394
Jhana <mrgamagejhana@gmail.com>
2023-10-27 16:46:19 +0200

    ja-vue

---
+++ ja-vue/src/components/WorkingTime.vue
@@ -0,0 +1,43 @@
+
+<template>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import axios from 'axios' 
+
+/**
+ * @component
+ * @description Used for displaying, creating, modifying and deleting a working time
+ * Do not confuse with WorkingTimes component
+ * linked to the routes:
+ * /workingTime/:userid (for creation) ;
+ * /workingTime/:userid/:workingtimeid (for modification and deletion).
+ */
+export default defineComponent({
+    name: 'WorkingTime',
+    components: {
+
+    },
+    data() {
+
+    },
+    methods: {
+        createWorkingTime() {
+
+        },
+        updateWorkingTime() {
+
+        },
+        deleteWorkingTime() {
+            
+        },
+    },
+    created() {
+        axios.get(process.env.API_URL + '/workingTimes/:userId');
+    },
+})
+</script>                       
+
+<style></style>
---
+++ ja-vue/src/components/bootstrap/CurrentWeather.vue
@@ -0,0 +1,58 @@
+
+<template>
+    <div>
+        <h2>
+            Météo actuelle à {{ weather.town }}
+        </h2>
+        Température: {{ weather.temperature }}°
+        <br>
+        Date: {{ weather.date }}
+    </div>
+    <ReloadButton :reload="reloadData"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ReloadButton from './ReloadButton.vue';
+
+// Bootstrap component
+export default defineComponent({
+
+    name: 'CurrentWeather',
+    components: {
+        ReloadButton
+    },
+    data() {
+        return {
+            weather: {
+                town: 'Brest',
+                temperature: 56,
+                date: getFrenchDate()
+            }
+        };
+    },
+    methods: {
+        reloadData() {
+            this.weather.temperature = randomNumber();
+        }
+    }
+})
+
+function getFrenchDate() :string{
+    const date = new Date();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
+}
+
+function randomNumber() :number{
+    const min = -20;
+    const max = 50;
+    const nombreAleatoire = Math.random() * (max - min) + min;
+    return Math.round(nombreAleatoire);
+}   
+
+</script>
+
+<style></style>
