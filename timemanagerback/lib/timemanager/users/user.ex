e993103b2d8ba0c5344daf5564474ea74428f2b5
Douggui <101191314+Douggui@users.noreply.github.com>
2023-11-06 11:09:06 +0100

    auth

--- timemanagerback/lib/timemanager/users/user.ex
+++ timemanagerback/lib/timemanager/users/user.ex
@@ -1,10 +1,12 @@
 defmodule Timemanager.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Bcrypt
 
   schema "users" do
     field :username, :string
     field :email, :string
+    field :password, :string
 
     timestamps(type: :utc_datetime)
   end
@@ -12,8 +14,15 @@ defmodule Timemanager.Users.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :email])
-    |> validate_required([:username, :email], message: "champs obligatoires")
+    |> cast(attrs, [:username, :email, :password])
+    |> validate_required([:username, :email, :password], message: "champs obligatoires")
     |> validate_format(:email,~r/^[A-Za-z0-9\._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}$/,message: "l'email n'est pas valide")
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password: Bcrypt.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
--- timemanagerback/lib/timemanager_web/auth/guardian.ex
+++ timemanagerback/lib/timemanager_web/auth/guardian.ex
@@ -1,16 +1,16 @@
-defmodule TimemanagerWeb.Auth.Guardian do
-use Guardian, opt_app: :auth_me
-alias Timemanager.Auth
+defmodule TimemanagerWeb.Users.Guardian do
+  use Guardian, opt_app: :timemanager
+  alias Timemanager.Users
 
-def subject_for_token(user, _claims) do
-  {:ok, to_string(user.id)}
-end
+  def subject_for_token(user, _claims) do
+    {:ok, to_string(user.id)}
+  end
 
-def resource_from_claims(%{"sub" => id}) do
-  user = Auth.get_user!(id)
-  {:ok, user}
-rescue
-  Ecto.NoResultsError -> {:error, :resource_not_found}
-end
+  def resource_from_claims(%{"sub" => id}) do
+    user = Users.get_user!(id)
+    {:ok, user}
+  rescue
+    Ecto.NoResultsError -> {:error, :resource_not_found}
+  end
 
-end
+  end
